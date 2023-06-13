b2963eab51d12feee835e0cbfb04447e9fabe3f4
iva28 <iva.hurt574@gmail.com>
2023-06-13 20:46:30 +0200

    Kostur projekta

---
+++ backend/app/Models/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use  HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'username',
+        'password',
+        'name',
+        'surname',
+        'date_birth',  
+    ];
+
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'date_birth' => "date"
+    ];
+
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
+
+    /** Treba mi friends */
+}
